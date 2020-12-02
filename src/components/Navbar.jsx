5da74b8a628b8346e722de9401af4de2ced7f608
eelok <eelok@mail.ru>
2020-12-02 15:50:47 +0100

    home page wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,17 @@ import {ReactComponent as XingLogo} from '../assets/xing.svg';
 import {ReactComponent as Github} from '../assets/github.svg';
 import {ReactComponent as Linkedin} from '../assets/linkedin.svg';
 
-import './button-stule.css';
+import './button-style.css';
 
 const Navbar = () => {
 
     return (
         <nav className='navbar'>
-            <Link to='#' className='navbar__logo'>eelok</Link>
+            <Link to='/' className='navbar__logo'>Eelok</Link>
             <div className='navbar__items'>
                 <Link to='/#' className='navbar__items-item'>About</Link>
                 <Link to='/#' className='navbar__items-item'>Projects</Link>
-                <Link to='/#' className='navbar__items-item btn-color-green contact'>Contact me</Link>
+                <Link to='/#' className='navbar__items-item btn btn-color-green contact'>Contact me</Link>
             </div>
             <div className='navbar__social'>
                 <XingLogo className='navbar__social-item'/>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
+import './homepage-style.css';
+import {Link} from "react-router-dom";
+
 
 const HomePage = () => {
     return (
         <div>
-        <Navbar/>
-        <h1>Home page</h1>
+            <Navbar/>
+            <section className='container'>
+                <div className='box-image'>
+
+                </div>
+                <div className='box-information'>
+                    <h4 className='secondary-heading'>Lorem ipsum dolor sit amet.</h4>
+                    <h1 className='main-heading'>Lorem ipsum dolor sit amet, consectetur</h1>
+                    <p className='text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, similique?</p>
+                    <Link to='/#' className='btn btn-color-violet'>Resume</Link>
+                </div>
+            </section>
         </div>
     )
 }
