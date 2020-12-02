f5c99cafbbb4a1b3cdcfe828b301b373ecf7c3bd
eelok <eelok@mail.ru>
2020-12-02 14:34:42 +0100

    header wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './navbar-style.css';
-import { ReactComponent as XingLogo } from '../assets/xing.svg';
-import { ReactComponent as Github } from '../assets/github.svg';
-import { ReactComponent as Linkedin } from '../assets/linkedin.svg';
+import {ReactComponent as XingLogo} from '../assets/xing.svg';
+import {ReactComponent as Github} from '../assets/github.svg';
+import {ReactComponent as Linkedin} from '../assets/linkedin.svg';
+
+import './button-stule.css';
 
 const Navbar = () => {
 
     return (
         <nav className='navbar'>
-            <Link to='#' className='logo'className='navbar__logo' >eelok</Link>
-            <div className='navbar__links'>
-                <Link to='/#' className='navbar__links-item'>About</Link>
-                <Link to='/#' className='navbar__links-item'>Projects</Link>
-                <Link to='/#' className='navbar__links-item btn-color-one'>Contact me</Link>
+            <Link to='#' className='navbar__logo'>eelok</Link>
+            <div className='navbar__items'>
+                <Link to='/#' className='navbar__items-item'>About</Link>
+                <Link to='/#' className='navbar__items-item'>Projects</Link>
+                <Link to='/#' className='navbar__items-item btn-color-green contact'>Contact me</Link>
             </div>
             <div className='navbar__social'>
-                <Link to='/#'>
-                    <Linkedin className='navbar__social-item'/>
-                </Link>
-                <Link to='/#'>
-                    <XingLogo className='navbar__social-item'/>
-                </Link>
-                <Link to='/#'>
-                    <Github className='navbar__social-item'/>
-                </Link>
+                <Linkedin className='navbar__social-item'/>
+                <Linkedin className='navbar__social-item'/>
+                {/*<XingLogo className='navbar__social-item'/>*/}
+                <Github className='navbar__social-item'/>
             </div>
         </nav>
     )
