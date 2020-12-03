30f7d629c8e8f3e7b1bc3ddf4f258f05ad30f769
wassertim <wassertim@gmail.com>
2020-12-03 20:22:59 +0100

    green tests

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 
 import React from "react";
 import HomePage from "./pages/HomePage";
-import {Switch, Route} from "react-router-dom";
+import Router, {Switch, Route} from "react-router-dom";
 
 function App() {
-    return (
-        <div className="App">
-            <Switch>
-                <Route exact path='/' component={HomePage}/>
-            </Switch>
-        </div>
-    );
+  return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path='/' component={HomePage}/>
+          </Switch>
+        </Router>
+      </div>
+  );
 }
 
 export default App;
