66b4858dc622bed4158b689bdec38fd42bd31318
eelok <eelok@mail.ru>
2020-12-05 15:40:21 +0100

    card wip

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './card-style.css';
-import {Link} from "react-router-dom";
+import { ReactComponent as GitHubLogo } from '../../assets/github.svg';
 
-const Card = ({title, description, technology}) => {
+const Card = ({title, technology}) => {
     return (
         <section className='card'>
             <div className='card__image-box'>&nbsp;</div>
             <div className='card__info'>
                 <h3 className='secondary-header'>{title}</h3>
-                <h5 className='third-header'>Description</h5>
-                <p className='plane-text'>{description}</p>
-                <h5 className='third-header'>Technology</h5>
-                <p className='plane-text'>{technology}</p>
+                <div className='card__info-technology'>
+                    {
+                        technology.map(item =>
+                            <div className='text-tag'>{item}</div>
+                        )
+                    }
+                </div>
                 <div className='card__control'>
-                    <button className='btn-round'>&#62;</button>
+                    <GitHubLogo className='link-button-to-social'/>
                 </div>
             </div>
         </section>
--- src/components/card/projectsData.js
+++ src/components/card/projectsData.js
@@ -2,19 +2,16 @@ export const PROJECTS_DATA = [
     {
         id: 1,
         title: 'Smilebusters',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet dolorum mollitia nesciunt quae veniam?',
         technology: ['React', 'html', 'css', 'firebase']
     },
     {
         id: 2,
         title: 'Admin-Smilebusters',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet dolorum mollitia nesciunt quae veniam?',
         technology: ['React', 'html', 'css', 'firebase']
     },
     {
         id: 3,
         title: 'Songs Microservices',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet dolorum mollitia nesciunt quae veniam?',
-        technology: ['Java', 'Spring', 'Spring Cloud', 'Hibernate', 'Testing with JUnit', 'Mockito', 'Eureka', 'Spring Cloud Gateway']
+        technology: ['Java', 'Spring', 'Spring Cloud', 'Hibernate', 'JUnit', 'Mockito', 'Eureka']
     },
 ]
