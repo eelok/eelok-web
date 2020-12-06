3ddfbd7dab1a6333a442a3cc538acb19d23c01ab
eelok <eelok@mail.ru>
2020-12-06 10:21:45 +0100

    about wip

--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
     return (
         <section className='about'>
             <div className='about__info-block'>
-                <h2 className='main-header'>Main Information aboyt Eelok</h2>
+                <h2 className='main-header'>Lorem ipsum dolor sit amet.</h2>
                 <p className='plane-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad aliquid
                     aperiam architecto at commodi debitis dolore dolores eos error impedit ipsa magni, optio quia
                     recusandae
