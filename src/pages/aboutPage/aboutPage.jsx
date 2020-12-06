9af1ef46e56f2b0cbfddb6cacb29745b77d5bdf4
eelok <eelok@mail.ru>
2020-12-06 16:03:44 +0100

    about style wip

--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -3,7 +3,7 @@ import './about-style.css';
 
 const AboutPage = () => {
     return (
-        <section className='about'>
+        <section className='about container'>
             <div className='about__info-block'>
                 <h2 className='main-header'>Lorem ipsum dolor sit amet.</h2>
                 <p className='plane-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad aliquid
