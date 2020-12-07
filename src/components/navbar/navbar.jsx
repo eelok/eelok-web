022a8b8f71094f6172e3ccfd1f55b01659b9c77b
eelok <eelok@mail.ru>
2020-12-07 17:16:35 +0100

    mivile nav wip

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,26 +6,29 @@ import {useState} from "react";
 
 const Navbar = () => {
 
-    const [menuIsOpen, setMenuIsOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setOpen(!open)
+    }
     return (
         <div className='container'>
             <nav className='navbar'>
-                <div className='hamburger' onClick={() => setMenuIsOpen(true)}>
+                <div className='hamburger' onClick={toggleMenu}>
                     <div className='line'></div>
                     <div className='line'></div>
                     <div className='line'></div>
                 </div>
-                <ul className={`nav-links ${menuIsOpen ? 'nav-links-open' : ''}`}>
-                    <li><a className='nav-links__item' href="#about"
-                        onClick={() => setMenuIsOpen(false)}
+                <ul className={`nav-links ${open ? 'nav-links-open' : ''}`}>
+                    <li className='nav-links-li'><a className='nav-links__item' href="#about"
+                        onClick={() => toggleMenu()}
                     >About</a></li>
-                    <li><a className='nav-links__item' href='#projects'
-                       onClick={() => setMenuIsOpen(false)}
+                    <li className='nav-links-li'><a className='nav-links__item' href='#projects'
+                       onClick={() => toggleMenu()}
                     >Projects</a></li>
-                    <li><a className='nav-links__item' href="#contact"
-                       onClick={() => setMenuIsOpen(false)}
-                    >Contact</a></li>
+                    <li className='nav-links-li btn btn-color-green'><a className='nav-links__item' href="#contact"
+                       onClick={() => toggleMenu()}
+                    >Contact Me</a></li>
                 </ul>
             </nav>
         </div>
