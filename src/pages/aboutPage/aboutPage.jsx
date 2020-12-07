3aa4f7342b5aadd588d840513ddd7b7d6fb7a3e0
eelok <eelok@mail.ru>
2020-12-07 15:38:32 +0100

    remove some code

--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -3,18 +3,21 @@ import './about-style.css';
 
 const AboutPage = () => {
     return (
-        <section className='about container' id='about'>
-            <div className='about__info-block'>
-                <h2 className='main-header'>Lorem ipsum dolor sit amet.</h2>
-                <p className='plane-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad aliquid
-                    aperiam architecto at commodi debitis dolore dolores eos error impedit ipsa magni, optio quia
-                    recusandae
-                    rerum sit, velit voluptatum.</p>
-            </div>
-            <div className='about__gallery-block'>
-                &nbsp;
-            </div>
-        </section>
+        <div className='container'>
+            <section className='about' id='about'>
+                <div className='about__info-block'>
+                    <h2 className='main-header'>Lorem ipsum dolor sit amet.</h2>
+                    <p className='plane-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad
+                        aliquid
+                        aperiam architecto at commodi debitis dolore dolores eos error impedit ipsa magni, optio quia
+                        recusandae
+                        rerum sit, velit voluptatum.</p>
+                </div>
+                <div className='about__gallery-block'>
+                    &nbsp;
+                </div>
+            </section>
+        </div>
     )
 
 }
