b5a60589418e8b6bb9d6df834f26f724baa922f6
eelok <eelok@mail.ru>
2020-12-07 12:17:09 +0100

    hamburger menu wip

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,11 +11,23 @@ const Navbar = () => {
         <div className='container'>
             <nav className='navbar'>
                 <Link to='/' className='navbar__logo'>Eelok</Link>
-                <div className='navbar__items'>
-                    <Link to='/about' className='navbar__items-item'>About</Link>
-                    <Link to='/projects' className='navbar__items-item'>Projects</Link>
-                    <Link to='/contact' className='navbar__items-item btn btn-color-green'>Contact me</Link>
+                <div className='mobileNav'>
+                    <div className='hamburger'>
+                        <div className='line'></div>
+                        <div className='line'></div>
+                        <div className='line'></div>
+                    </div>
+                    <ul className="nav-links">
+                        <li><a className='nav-links__item' href="#">About</a></li>
+                        <li><a className='nav-links__item' href="#">Projects</a></li>
+                        <li><a className='nav-links__item' href="#">Contact</a></li>
+                    </ul>
                 </div>
+                {/*<div className='navbar__items'>*/}
+                {/*    <Link to='/about' className='navbar__items-item'>About</Link>*/}
+                {/*    <Link to='/projects' className='navbar__items-item'>Projects</Link>*/}
+                {/*    <Link to='/contact' className='navbar__items-item btn btn-color-green'>Contact me</Link>*/}
+                {/*</div>*/}
                 <Social/>
             </nav>
         </div>
