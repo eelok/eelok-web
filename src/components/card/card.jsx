776556ec27d1aa2e06992a8717d686a8d31f3bc4
eelok <eelok@mail.ru>
2020-12-18 21:48:22 +0100

    change card wip

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './card-style.css';
 import {ReactComponent as GitHubLogo} from '../../assets/github.svg';
 
-const Card = ({title, technology}) => {
+const Card = ({title, technology, imageUrl}) => {
     return (
         <section className='card'>
-            <div className='card__image-box'>&nbsp;</div>
+            <div className='card__image-box'>
+                <img className='card__image' src={imageUrl} alt=""/>
+            </div>
             <div className='card__info'>
                 <h3 className='secondary-header'>{title}</h3>
                 <div className='card__info-technology'>
--- src/components/card/projectsData.js
+++ src/components/card/projectsData.js
@@ -1,17 +1,23 @@
+import smilebusters from '../../assets/smilebusters.png';
+import adminSmilebusters from '../../assets/admin-smilebusters.png';
+
 export const PROJECTS_DATA = [
     {
         id: 1,
         title: 'Smilebusters',
-        technology: ['React', 'html', 'css', 'firebase']
+        technology: ['React', 'html', 'css', 'firebase'],
+        imageUrl: smilebusters
     },
     {
         id: 2,
         title: 'Admin-Smilebusters',
-        technology: ['React', 'html', 'css', 'firebase']
+        technology: ['React', 'html', 'css', 'firebase'],
+        imageUrl: adminSmilebusters
     },
     {
         id: 3,
         title: 'Songs Microservices',
-        technology: ['Java', 'Spring', 'Spring Cloud', 'Eureka', 'Hibernate', 'JUnit', 'Mockito']
+        technology: ['Java', 'Spring', 'Spring Cloud', 'Eureka', 'Hibernate', 'JUnit', 'Mockito'],
+        imageUrl: ''
     },
 ]
