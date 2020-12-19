814a7fce3979d28b550e5f4c9f270564eb7ce369
eelok <eelok@mail.ru>
2020-12-19 17:08:02 +0100

    add support of projects img for different screen density

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,10 +3,15 @@ import './card-style.css';
 import {ReactComponent as GitHubLogo} from '../../assets/github.svg';
 
 const Card = ({title, technology, imageUrl, gitHubUrl}) => {
+    const [imageUrl1x, imageUrl2x] = imageUrl;
+
     return (
         <section className='card'>
             <div className='card__image-box'>
-                <img className='card__image' src={imageUrl} alt=""/>
+                <img className='card__image'
+                     srcSet={`${imageUrl1x} 1x, ${imageUrl2x} 2x`}
+                     alt="project cover"
+                />
             </div>
             <div className='card__info'>
                 <h3 className='secondary-header'>{title}</h3>
