135c30f3f0e143769bf60b150dc9ba0b2bbfd728
eelok <eelok@mail.ru>
2020-12-19 21:46:41 +0100

    add more icons to skill set

--- src/components/qualification/qualification.jsx
+++ src/components/qualification/qualification.jsx
@@ -1,25 +1,55 @@
 import React from 'react';
-// import {ReactComponent as MockitoIcon} from '../../assets/qualiIcons/Mockito_Logo.png'; /
-import { ReactComponent as HtmlIcon } from '../../assets/qualiIcons/html-5.svg';
-import { ReactComponent as CSSIcon } from '../../assets/qualiIcons/css.svg';
-import { ReactComponent as ReactIcon } from '../../assets/qualiIcons/react001.svg';
-import { ReactComponent as HibernateIcon } from '../../assets/qualiIcons/hibernate-icon.svg';
-import { ReactComponent as JavaIcon } from '../../assets/qualiIcons/java.svg';
-import { ReactComponent as FirebaseIcon} from '../../assets/qualiIcons/firebase.svg';
+
+import htmlicon from '../../assets/qualiIcons/html.svg';
+import cssicon from '../../assets/qualiIcons/css.svg';
+import reacticon from '../../assets/qualiIcons/reactIcon.svg';
+import hibernateicon from '../../assets/qualiIcons/hibernate.svg';
+import javaicon from '../../assets/qualiIcons/java.svg';
+import firebaseicons from '../../assets/qualiIcons/google-firebase.svg';
+import springicon from '../../assets/qualiIcons/spring-icon.svg';
+import mockitoicon from '../../assets/qualiIcons/mockito_Logo.png';
+import jUniticon from '../../assets/qualiIcons/jUnit-icon.png';
+import postgresql from '../../assets/qualiIcons/postgresql.svg';
+import gitIcon from '../../assets/qualiIcons/git-icon.svg';
+import mavenIcon from '../../assets/qualiIcons/Maven_logo.svg';
+import intelliJ from '../../assets/qualiIcons/IntelliJ_IDEA.svg';
+import visualStudioCode from '../../assets/qualiIcons/Visual_Studio_Code.svg';
+
 import './qualification-style.css';
 
-const Qualification = () =>{
+const Qualification = () => {
+    const icons = [
+        {title: 'HTML', icon: htmlicon},
+        {title: 'CSS', icon: cssicon},
+        {title: 'React', icon: reacticon},
+        {title: 'Java', icon: javaicon},
+        {title: 'Spring', icon: springicon},
+        {title: 'Hibernate', icon: hibernateicon},
+        {title: 'Firebase', icon: firebaseicons},
+        {title: 'PostgreSQL', icon: postgresql},
+        {title: 'Mockito', icon: mockitoicon},
+        {title: 'jUnit', icon: jUniticon},
+        {title: 'Git', icon: gitIcon},
+        {title: 'Maven', icon: mavenIcon},
+        {title: 'Intellij IDEA', icon: intelliJ},
+        {title: 'Visual Studio Code', icon: visualStudioCode},
+    ];
     return (
-        <div>
-            {/*<MockitoIcon/>*/}
-            <HtmlIcon  className='qualification__item'/>
-            <CSSIcon className='qualification__item'/>
-            <ReactIcon className='qualification__item'/>
-            <HibernateIcon className='qualification__item'/>
-            <JavaIcon className='qualification__item' />
-            <FirebaseIcon className='qualification__item'/>
-
-        </div>
+        <section>
+            <h3 className='secondary-header tech-header'>Skill Set</h3>
+            <div className='tech-list'>
+                {icons.map((tech) => (
+                    <div className='tech'>
+                        <img
+                            src={tech.icon}
+                            alt={tech.title}
+                            className='tech__icon'
+                        />
+                        <span className='tech__name text-tag'>{tech.title}</span>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }
 export default Qualification;
--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
         <div className='container'>
             <section className='about' id='about'>
                 <div className='about__info-block'>
-                    <h2 className='main-header'>About Me.</h2>
+                    <h2 className='main-header header-about'>About Me.</h2>
                     <h3 className='third-header'>I'm a software developer based in Berlin, Germany.</h3>
                     <p className='plane-text'>I enjoy turning complex problems into simple, beautiful and intuitive
                         designs. When I'm not coding, tweeting or pushing pixels, you'll find me cooking, gardening or
