75d68259e0b114b4fe2701f4c3877913325fa67e
eelok <eelok@mail.ru>
2020-12-20 19:26:32 +0100

    clean up code in card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,17 +2,10 @@ import React from 'react';
 import './card-style.css';
 import {ReactComponent as GitHubLogo} from '../../assets/github.svg';
 
-const Card = ({title, technology, imageUrl, gitHubUrl}) => {
-    const [imageUrl1x, imageUrl2x] = imageUrl;
+const Card = ({title, technology, gitHubUrl}) => {
 
     return (
         <section className='card'>
-            <div className='card__image-box'>
-                {/*<img className='card__image'*/}
-                {/*     srcSet={`${imageUrl1x} 1x, ${imageUrl2x} 2x`}*/}
-                {/*     alt="project cover"*/}
-                {/*/>*/}
-            </div>
             <div className='card__info'>
                 <h3 className='secondary-header'>{title}</h3>
                 <div className='card__info-technology'>
--- src/components/card/projectsData.js
+++ src/components/card/projectsData.js
@@ -1,21 +1,15 @@
-import smilebusters2x from '../../assets/smilebusters.jpg';
-import smilebusters1x from '../../assets/smilebusters@0.5x.jpg';
-import adminSmilebusters2x from '../../assets/admin-smilebusters.jpg';
-import adminSmilebusters1x from '../../assets/admin-smilebusters@0.5x.jpg';
 
 export const PROJECTS_DATA = [
     {
         id: 1,
         title: 'Smilebusters',
         technology: ['React', 'html', 'css', 'firebase'],
-        imageUrl: [smilebusters1x, smilebusters2x],
         gitHubUrl: 'https://github.com/eelok/smilebusters'
     },
     {
         id: 2,
         title: 'Admin-Smilebusters',
         technology: ['React', 'html', 'css', 'firebase'],
-        imageUrl: [adminSmilebusters1x, adminSmilebusters2x],
         gitHubUrl: 'https://github.com/eelok/admin-office-sb'
     },
     // {
