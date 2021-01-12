d93eb7258354e4d40ca787df57c5e4554c753f71
eelok <eelok@mail.ru>
2021-01-12 10:52:53 +0100

    add location to useEffec

--- src/App.js
+++ src/App.js
@@ -2,39 +2,31 @@ import './App.css';
 
 import React from "react";
 import HomePage from "./pages/homePage/homePage";
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, useLocation} from "react-router-dom";
 import {MenuProvider} from "./context/AllStateContext";
 import ResumePage from "./pages/resume/resume";
 import ReactGA from "react-ga";
 import {useEffect} from 'react';
-import {createBrowserHistory} from 'history';
-import { Router } from 'react-router-dom';
 
 function App() {
-    const history = createBrowserHistory();
+
+    let location = useLocation();
+    ReactGA.initialize('UA-187055448-1');
 
     useEffect(() => {
-        ReactGA.initialize('UA-187055448-1');
-        ReactGA.pageview(window.location.pathname);
-    });
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, [location]);
 
-    history.listen(location => {
-        ReactGA.initialize('UA-187055448-1');
-        ReactGA.set({page: location.pathname});
-        ReactGA.pageview(location.pathname);
-    })
 
     return (
-        <Router history={history}>
-            <MenuProvider>
-                <div className="App">
-                    <Switch>
-                        <Route exact path='/' component={HomePage}/>
-                        <Route exact path='/cv' component={ResumePage}/>
-                    </Switch>
-                </div>
-            </MenuProvider>
-        </Router>
+        <MenuProvider>
+            <div className="App">
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route exact path='/cv' component={ResumePage}/>
+                </Switch>
+            </div>
+        </MenuProvider>
     );
 }
 
