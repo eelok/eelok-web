a96933f8b94ea997f3cd95d01c6dd72e4c180de1
eelok <eelok@mail.ru>
2021-01-12 09:56:33 +0100

    google analytics

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import HomePage from "./pages/homePage/homePage";
 import {Switch, Route} from "react-router-dom";
 import {MenuProvider} from "./context/AllStateContext";
 import ResumePage from "./pages/resume/resume";
+import ReactGA from "react-ga";
+import {useEffect} from 'react';
+// import { createBrowserHistory } from 'history';
 
 function App() {
+    // const history = createBrowserHistory();
+
+    useEffect(() => {
+        ReactGA.initialize('UA-187055448-1');
+        ReactGA.pageview(window.location.pathname);
+    });
+
     return (
         <MenuProvider>
             <div className="App">
