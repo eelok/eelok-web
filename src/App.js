ddcacf03c07f4c4e26f0f74022b2ac6cdde09dc9
eelok <eelok@mail.ru>
2021-01-12 10:20:11 +0100

    add history

--- src/App.js
+++ src/App.js
@@ -7,25 +7,34 @@ import {MenuProvider} from "./context/AllStateContext";
 import ResumePage from "./pages/resume/resume";
 import ReactGA from "react-ga";
 import {useEffect} from 'react';
-// import { createBrowserHistory } from 'history';
+import {createBrowserHistory} from 'history';
+import { Router } from 'react-router-dom';
 
 function App() {
-    // const history = createBrowserHistory();
+    const history = createBrowserHistory();
 
     useEffect(() => {
         ReactGA.initialize('UA-187055448-1');
         ReactGA.pageview(window.location.pathname);
     });
 
+    history.listen(location => {
+        ReactGA.initialize('UA-187055448-1');
+        ReactGA.set({page: location.pathname});
+        ReactGA.pageview(location.pathname);
+    })
+
     return (
-        <MenuProvider>
-            <div className="App">
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route exact path='/cv' component={ResumePage}/>
-                </Switch>
-            </div>
-        </MenuProvider>
+        <Router history={history}>
+            <MenuProvider>
+                <div className="App">
+                    <Switch>
+                        <Route exact path='/' component={HomePage}/>
+                        <Route exact path='/cv' component={ResumePage}/>
+                    </Switch>
+                </div>
+            </MenuProvider>
+        </Router>
     );
 }
 
