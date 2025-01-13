9354619061f1434fdcda6d2aaca92cb17e79eb6b
maria <m.petri@ackerpause.de>
2025-01-13 18:01:14 +0100

    change context

--- src/pages/aboutPage/aboutPage.jsx
+++ src/pages/aboutPage/aboutPage.jsx
@@ -9,10 +9,9 @@ const AboutPage = () => {
             <section className='about' id='about'>
                 <div className='about__info-block'>
                     <h2 className='main-header header-about'>About Me</h2>
-                    <h3 className='third-header'>I'm a software developer based in Berlin, Germany.</h3>
-                    <p className='plane-text'>I enjoy learning new technologies and computer languages.
-                        When I'm not coding you'll find me at a fitness club. I also like to discover new restaurants
-                        and delicious food.</p>
+                    <h3 className='third-header'>I’m a passionate software developer specializing in building efficient and innovative backend systems.</h3>
+                    <p className='plane-text'>I have a curious mindset that drives me to explore emerging technologies and refine my technical skills.
+                        Outside of development, I enjoy staying active through outdoor activities like cycling and running. I’m also a food enthusiast who loves trying new cuisines and discovering hidden culinary gems.</p>
                 </div>
                 <img className='about-separator'
                      src={separator} alt=""/>
--- src/pages/resume/resume-data.js
+++ src/pages/resume/resume-data.js
@@ -6,12 +6,12 @@ import {ReactComponent as internet}    from '../../assets/internet.svg';
 
 export const  RESUME_DATA = [
     {
-        name: 'Maria Petretckaia',
+        name: 'Maria Petri',
         activity: 'Software Developer',
         links: [
             {
                 icon: homeAddress,
-                link: {title:'Germany, Waldshut-Tiengen'}
+                link: {title:'Switzerland; Remote-Friendly'}
             },
             {
                 icon: linkedin,
@@ -40,20 +40,35 @@ export const  RESUME_DATA = [
         ],
         workExperience: [
             {
-                time: '2021-12 - present',
-                companyName: 'Resourceful Humans GmbH',
-                position: 'Junior Backend Node.JS Software Developer',
-                intro: 'I introduced tests and kept the culture of writing tests for each feature and sought toward full coverage. ' +
-                    'I implemented new features and refactored code to make it more maintainable and readable.',
-                duty: ['Implementation of GraphQL API',
-                      'Utilising Sequelize ORM',
-                      'Store and fetch data in and from Postgres',
-                      'Writing unit and integration tests with Jest',
-                      'Github, git', 'Jira, Notion']
+                time: "2023-06 - present",
+                companyName: "AckerCompany GmbH, Germany",
+                position: "Backend Software Developer",
+                duty: [
+                    'Refactoring and optimizing existing systems to enhance efficiency and functionality.',
+                    'Implementation of unit and integration tests to ensure software reliability, maintainability, and long-term stability.',
+                    'Working with the onion architecture to maintain a modular, maintainable, and scalable codebase.',
+                    'Implementation and managing microservices architectures o enhance system scalability, resilience, and adaptability to evolving requirements.',
+                    'Implementation of new features in REST API',
+                    'Collaborating with cross-functional teams.',
+                    'Participating in code reviews.'
+                ]
+            },
+            {
+                time: '2021-12 - 2023-05',
+                companyName: 'Resourceful Humans GmbH, Germany',
+                position: 'Junior Backend Software Developer',
+                duty: ['Implementation of new features in the GraphQL API.',
+                       'Refactoring code to enhance maintainability, readability, and stability.',
+                       'Configuration of GitHub Actions workflows for building, testing, and deploying code.',
+                       'Performing Git operations such as merging, rebasing, and cherry-picking using Git and GitHub.',
+                       'Development of database functionality using Sequelize and raw SQL queries.',
+                       'Implementation of unit and integration tests with Jest.',
+                       'Testing APIs and endpoints using Postman.',
+                       'Agile collaboration with ticketing systems like Jira and Notion.']
             }
         ],
-        skills: ['JavaScript', 'Java', 'React', 'Spring Boot', 'Spring Cloud',  'HTML', 'CSS','Jest', 'Junit', 'Mockito','Sequelize', 'Hibernate', 'PostgreSQL', 'Git', 'Maven', 'Postman', 'Jira', 'Notion'],
-        languages: ['German (C1 certificate)', 'English (good written and spoken)', 'Russian (native)'],
+        skills: ['JavaScript', 'TypeSCript', 'Java', 'Express', 'React', 'Spring Boot', 'HTML','Jest', 'Junit', 'Mockito','Sequelize', 'Hibernate', 'PostgreSQL', 'Git', 'Maven', 'Postman', 'Bruno', 'Jira', 'Notion'],
+        languages: ['German', 'English', 'Russian (native)'],
         softSkills: ['Teamwork', 'Flexibility', 'Time management', 'Responsibility'],
     }
 ]
